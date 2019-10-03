1dc1e678e4b214bcf20ac7022b7d8dc5448ae5eb
Annie <annie.yiyunc@gmail.com>
2019-10-02 19:48:44 -0700

    grab input from form and push to database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,43 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA8lVbDKOGTcVzCI4lZu-lcWg-te2PKa6g",
+    authDomain: "test-f22a8.firebaseapp.com",
+    databaseURL: "https://test-f22a8.firebaseio.com",
+    projectId: "test-f22a8",
+    storageBucket: "test-f22a8.appspot.com",
+    messagingSenderId: "713836356992",
+    appId: "1:713836356992:web:e15611503c0871c6568d65"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+
 $(document).ready(
     function employeeTracker() {
-        var employeeName
-        var role
-        var startDate
-        var monthleyRate
         
         // when submit button click
         $("#submit-button").on("click", function (e) {
             e.preventDefault();
             // get the values from input field
-            employeeName = $("#employee-name").val().trim();
+            var employeeName = $("#employee-name").val().trim();
             console.log(employeeName);
-            role = $("#role").val().trim();
+            var role = $("#role").val().trim();
             console.log(role);
-            startDate = $("#start-date").val().trim();
+            var startDate = $("#start-date").val().trim();
             console.log(startDate);
-            monthleyRate = $("#monthley-rate").val().trim();
+            var monthleyRate = $("#monthley-rate").val().trim();
             console.log(monthleyRate);
+            
+            database.ref().push({
+                name: employeeName,
+                role: role,
+                startDate: startDate,
+                monthleyRate: monthleyRate
+            })
         })
+
+        
 
     }
 
